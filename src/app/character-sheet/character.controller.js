443acec898cd95014dd920069b96470380ee659a
devin <devin@thewebdev.ca>
2015-12-28 20:07:02 -0800

    adding untracked changes

--- src/app/character-sheet/character.controller.js
+++ src/app/character-sheet/character.controller.js
@@ -6,50 +6,50 @@
     .controller('CharacterController', CharacterController);
 
   /** @ngInject */
-    function CharacterController($stateParams, characterService, proficiencyBonusService, basicInfoService, $log, $scope) {
-        var vm = this;
-        var characterId = $stateParams.characterId;
+  function CharacterController($stateParams, characterService, proficiencyBonusService, basicInfoService, $log, $scope) {
+    var vm = this;
+    var characterId = $stateParams.characterId;
 
-        //Get a character based on id
-        vm.getCharacter = function(characterId){
-          characterService.getCharacter(characterId).then(function(character){
-            vm.character = character;
-            vm.getSpeed(vm.character.race);
-            vm.getHitDice(vm.character.class);
-          });
-        };
+    //Get a character based on id
+    vm.getCharacter = function(characterId) {
+      characterService.getCharacter(characterId).then(function(character) {
+        vm.character = character;
+        vm.getSpeed(vm.character.race);
+        vm.getHitDice(vm.character.class);
+      });
+    };
 
-        //Update the character
-        vm.updateCharacter = function(id, object){
-          characterService.updateCharacter(id, object);
-        };
+    //Update the character
+    vm.updateCharacter = function(id, object) {
+      characterService.updateCharacter(id, object);
+    };
 
-        //Get the characters proficiency bonus
-        vm.getProficiencyBonus = function(level){
-          vm.character.proficiencyBonus = proficiencyBonusService.getProficiencyBonus(level);
-        };
+    //Get the characters proficiency bonus
+    vm.getProficiencyBonus = function(level) {
+      vm.character.proficiencyBonus = proficiencyBonusService.getProficiencyBonus(level);
+    };
 
-        //Get character speed based on race
-        vm.getSpeed = function(race){
-          basicInfoService.getSpeed(race).then(function(speed){
-            vm.character.speed = speed;
-            $scope.$digest();
-          });
-        };
+    //Get character speed based on race
+    vm.getSpeed = function(race) {
+      basicInfoService.getSpeed(race).then(function(speed) {
+        vm.character.speed = speed;
+        $scope.$digest();
+      });
+    };
 
-        //Get the hit dice for the class
-        vm.getHitDice = function(className){
-          basicInfoService.getHitDice(className).then(function(hitDice){
-            vm.character.hitDice = hitDice;
-            $scope.$digest();
-          });
-        };
+    //Get the hit dice for the class
+    vm.getHitDice = function(className) {
+      basicInfoService.getHitDice(className).then(function(hitDice) {
+        vm.character.hitDice = hitDice;
+        $scope.$digest();
+      });
+    };
 
-        activate();
+    activate();
 
-        function activate() {
-          $log.log('character id = ' + characterId);
-          vm.getCharacter(characterId);
-        }
+    function activate() {
+      $log.log('character id = ' + characterId);
+      vm.getCharacter(characterId);
     }
+  }
 })();
