230573a89c6b51a1896d7ee17d58327979d82237
devin <devin@thewebdev.ca>
2016-01-07 23:01:47 -0800

    new build

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'dc',
+      module: 'templateCache',
       standalone:true,
       root: 'app'
     }))
