7453f7b9bd95e34202f325983e7121380a57d777
devin <devin@thewebdev.ca>
2016-02-01 19:52:47 -0800

    fixed remove item tracking

--- src/app/character-sheet/weapons/weapons.controller.js
+++ src/app/character-sheet/weapons/weapons.controller.js
@@ -13,7 +13,7 @@
 
 
     //Show the weapons dialog
-    vm.showWeaponsDialog = function (ev, proficiencyBonus, statMods) {
+    vm.showWeaponsDialog = function (ev, character, statMods) {
       $mdDialog.show({
         controller: WeaponsDialogController,
         templateUrl: 'app/character-sheet/weapons/weapons.dialog.html',
@@ -22,17 +22,21 @@
         clickOutsideToClose: true,
         controllerAs: 'dialog',
         locals: {
-          proficiencyBonus: proficiencyBonus,
+          character: character,
           statMods: statMods
         }
       });
     };
 
     //Weapons Dialog controller
-    function WeaponsDialogController($mdDialog, proficiencyBonus, statMods, $log, pouchService, autocompleteService, $filter) {
+    function WeaponsDialogController($mdDialog, character, statMods, $log, pouchService, autocompleteService, $filter) {
       var vm = this;
       vm.weaponsList = [];
       vm.searchText = '';
+      // vm.weapons = [];
+      vm.myOrder = 'name';
+      vm.character = character;
+      vm.selected = [];
       vm.cancel = function () {
         $mdDialog.cancel();
       };
@@ -63,12 +67,80 @@
           }
           return results;
         }
+        vm.addWeapon = function (weaponName) {
+          var params = {
+            selector: {
+              type: 'weapon',
+              name: weaponName
+            },
+            fields: ['name', '_id', 'weaponType', 'cost', 'damage', 'damageType', 'properties', 'weight'],
+            // include_docs: true
+          };
+          pouchService.query(params).then(function (response) {
+
+            vm.character.weapons.push(response[0]);
+
+            pouchService.put(vm.character).then(function(update){
+              vm.character._rev = update.rev;
+            });
+          });
+        }
+
+        vm.showWeaponDetails = function (weaponName) {
+          var params = {
+            selector: {
+              type: 'weapon',
+              name: weaponName
+            },
+            fields: ['name', '_id', 'weaponType', 'cost', 'damage', 'damageType', 'properties', 'weight'],
+            // include_docs: true
+          };
+
+          pouchService.query(params).then(function (response) {
+            $log.log(response);
+            vm.weaponDetails = response[0];
+          });
+        }
+
+        vm.removing = {};
+        vm.showActions = function (weapon) {
+          weapon.showActions = true;
+          // vm.removing[index] = true;
+        };
+
+        //Hide equiped weapon actions
+        vm.hideActions = function (weapon) {
+          weapon.showActions = false;
+          // vm.removing[index] = false;
+        };
+
+        //Clear weapon details
+        vm.clearWeaponDetails = function () {
+          vm.weaponDetails = null;
+        };
+
+        //Remove weapons from the list
+        vm.removeWeapon = function (index) {
+          var array = $filter('orderBy')(vm.character.weapons, vm.myOrder);
+
+          vm.removing[index] = false;
+          array.splice(index, 1);
+          vm.character.weapons = array;
+
+          pouchService.put(vm.character).then(function(update){
+            vm.character._rev = update.rev;
+          });
+        };
       };
 
       activate();
 
       function activate() {
         vm.getAllWeapons();
+
+        if(!vm.character.weapons) {
+          vm.character.weapons = [];
+        }
       }
     }
 
